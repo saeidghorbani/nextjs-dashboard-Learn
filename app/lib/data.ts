3dfe2053c884ab53964b8a570510fa879f9dfa3b
saeidghorbani <saeidsemail@gmail.com>
2025-12-12 18:26:06 +0330

    feat: Chapter 9 Streaming
    Streaming: a data transfer technique that allows you to break down a route into smaller "chunks" and progressively stream them from the server to the client as they become ready. This allows the user to se
    way: loading.tsx for page level streaming, move data fetches down to the components that need it and then wrap those components in Suspense

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,13 +32,15 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // console.log("Fetching latestInvoices data...");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+    // console.log("latestInvoices fetch completed after 1 seconds.");
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
@@ -52,6 +54,8 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+    // console.log("Fetching CardData data...");
+    // await new Promise((resolve) => setTimeout(resolve, 500));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
@@ -68,6 +72,7 @@ export async function fetchCardData() {
       invoiceStatusPromise,
     ]);
 
+    // console.log("fetchCardData fetch completed after 0.5 seconds.");
     const numberOfInvoices = Number(data[0][0].count ?? "0");
     const numberOfCustomers = Number(data[1][0].count ?? "0");
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? "0");
