3efffa21d2e0c6b94d01d6f3280121d182198fae
saeidghorbani <saeidsemail@gmail.com>
2025-12-19 00:39:10 +0330

    fix: add login username and password for testing

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -24,8 +24,11 @@ export default function LoginForm() {
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Use the following credentials(for testing):
         </h1>
+        <p>Email: user@nextmail.com</p>
+        <p>Password: 123456</p>
+
         <div className="w-full">
           <div>
             <label
