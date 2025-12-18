ccb1ffc4a62dfae08be0171df920b8afd4dc09e6
saeidghorbani <saeidsemail@gmail.com>
2025-12-18 12:09:09 +0330

    feat: Completed Chapter 12 Handling Errors
    -use try-catch for server actions.
    -define error.tsx for uncaught exceptions in server actions and rendering
    -define not-found.tsx and use notFound() function for not founded resources

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,12 +1,19 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const { id } = await props.params;
 
-  const customers = await fetchCustomers();
-  const invoice = await fetchInvoiceById(id);
+  const [customers, invoice] = await Promise.all([
+    fetchCustomers(),
+    fetchInvoiceById(id),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
